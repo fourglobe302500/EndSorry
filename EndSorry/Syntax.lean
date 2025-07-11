33ea27d03e7f2f867a41819eea722823d7cc17b4
fourglobe <franciscomiguelolima@gmail.com>
2025-07-11 17:51:31 +0100

    Semi-Functioning recursive and suggestion & partial implication

--- EndSorry/Syntax.lean
+++ EndSorry/Syntax.lean
@@ -8,23 +8,87 @@ syntax (name := endSorry) "sorry?" : tactic
 
 set_option pp.all true
 
-elab_rules : tactic
-  | `(tactic| sorry?) =>
-    withMainContext do
-      let goal ← getMainGoal
-      let type ← goal.getType
-      let ref ← getRef
+def letters := #["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
-      match ← inferTypeQ type with
-      | ⟨1, ~q(Prop), ~q($a = $b)⟩ =>
-        Lean.logInfo s!"By Eq: {a}"
-        Lean.logInfo s!"By Eq: {b}"
-        if ← isDefEq a b then
-          goal.refl
-          addTermSuggestion ref (.const `rfl [])
-        else
-          return ← admitGoal goal
-      | ⟨_u, _α, a⟩ =>
-        Lean.logInfo s!"By default: {a}"
-        addTermSuggestion ref (.const `sorry [])
-        admitGoal goal
+def getName (n : Nat) : Nat × String :=
+  let base := letters[n % letters.size]!
+  let rest := n / letters.size
+
+  if rest > 0 then
+    (n+1, s!"{base}{rest}")
+  else
+    (n+1, base)
+
+set_option autoImplicit false
+
+partial def loop (n := 0) : TacticM (String)  :=
+    withMainContext do
+
+      try
+        let goal ← getMainGoal
+
+        let type ← goal.getType
+
+        match ← inferTypeQ type with
+        | ⟨1, ~q(Prop), ~q($a = $b) ⟩ =>
+          if ← isDefEq a b then
+            goal.refl
+            return "rfl"
+          else
+            admitGoal goal
+            return "sorry"
+        | ⟨1, ~q(Prop), ~q(True) ⟩ =>
+          let intro := (.const `True.intro [])
+          let goals ← goal.apply intro
+          replaceMainGoal goals
+          return "apply True.intro"
+        | ⟨1, ~q(Prop), ~q((_ : ($a : Prop)) → ($b : Prop))⟩ =>
+          let (n, name) := getName n
+          let (_v, newGoal) ← goal.intro name.toName
+          replaceMainGoal [newGoal]
+          return s!"intro {name}\n{← loop n}"
+        | ⟨1, ~q(Prop), ~q((_ : ($a : Type)) → ($b : Prop))⟩ =>
+          let (n, name) := getName n
+          let (_v, newGoal) ← goal.intro name.toName
+          replaceMainGoal [newGoal]
+          return s!"intro {name}\n{← loop n}"
+        | ⟨2, ~q(Type), ~q((_ : ($a : Type)) → ($b : Type))⟩ =>
+          let (n, name) := getName n
+          let (_v, newGoal) ← goal.intro name.toName
+          replaceMainGoal [newGoal]
+          return s!"intro {name}\n{← loop n}"
+        | ⟨1, ~q(Prop), ~q(¬$a) ⟩ =>
+          let (n, name) := getName n
+          let (_v, newGoal) ← goal.intro name.toName
+          replaceMainGoal [newGoal]
+          return s!"intro {name}\n{← loop n}"
+        | ⟨1, ~q(Prop), a⟩ =>
+          let ctx ← getLCtx
+          let option_matching_expression ← ctx.findDeclM? fun decl => do
+            let declExpr := decl.toExpr
+            let declType ← inferType declExpr
+            if ← isDefEq declType type
+              then return some (declExpr, decl.userName)
+              else return none
+          match option_matching_expression with
+          | none =>
+            Lean.logInfo s!"By default: {a}"
+            admitGoal goal
+            return "sorry"
+          | some (e, userName) =>
+            closeMainGoal `endSorry e
+            return s!"exact {userName}"
+      catch _e =>
+        return ""
+
+elab_rules : tactic
+  | `(tactic| sorry?) => do
+    let ref ← getRef
+    let raw ← loop
+
+    let text := SuggestionText.string raw
+    let suggestion : Suggestion := {suggestion := text}
+
+    addSuggestion ref suggestion
+
+    return
--- EndSorryTest/trivial.lean
+++ EndSorryTest/trivial.lean
@@ -1,6 +1,51 @@
 import EndSorry
 
-set_option pp.all true
+-- set_option pp.all true
+set_option autoImplicit false
 
-theorem reflection : a = a := by
+theorem reflection {α : Type} (a : α) : a = a := by
+  -- rfl
+  sorry?
+
+theorem true : True := by
+  -- apply True.intro
+  sorry?
+
+theorem false : ¬False := by
+  -- intro a
+  -- exact a
+  sorry?
+
+theorem a_true {α : Type} : α → True := by
+  -- intro a
+  -- apply True.intro
+  sorry?
+
+theorem a_then_a {α : Prop} : α → α := by
+  -- intro a
+  -- exact a
+  sorry?
+
+theorem a_b_true {α β : Type} : α → β → True := by
+  -- intro a
+  -- intro b
+  -- apply True.intro
+  sorry?
+
+theorem a_b_then_a {β : Type} {α : Prop} : α → β → α := by
+  -- intro a
+  -- intro b
+  -- exact a
+  sorry?
+
+theorem a_b_then_b {α : Type} {β : Prop} : α → β → β := by
+  -- intro a
+  -- intro b
+  -- exact b
+  sorry?
+
+theorem a_a : ∃ α, α  → True := by
+  sorry?
+
+theorem a_b : ∀ α, α  → True := by
   sorry?
