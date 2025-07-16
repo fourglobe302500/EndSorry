422c7cfc5e39497728bf13b09c5a6361db77a319
fourglobe <franciscomiguelolima@gmail.com>
2025-07-16 12:49:35 +0100

    Adding simple And, and refactoring code for the recursive step

--- EndSorry/Syntax.lean
+++ EndSorry/Syntax.lean
@@ -28,75 +28,85 @@ def Lean.Expr.matchArrow? (e : Expr): Option (Expr × Expr) :=
 
 set_option autoImplicit false
 
+def finished : TacticM Bool := do
+  return (← getUnsolvedGoals).length == 0
+
+def process (goal : MVarId) (n : Nat) : TacticM (String × Nat) := do
+  let type ← goal.getType
+  match ← inferTypeQ type with
+  | ⟨1, ~q(Prop), ~q($a = $b) ⟩ =>
+    if ← isDefEq a b then
+      goal.refl
+      return ("rfl", n)
+    else
+      admitGoal goal
+      return ("sorry", n)
+  | ⟨1, ~q(Prop), ~q($a ∧ $b) ⟩ =>
+    let newGoals ← goal.constructor
+    replaceMainGoal newGoals
+    -- logInfo m!"{newGoals}"
+    return ("constructor", n)
+  | ⟨1, ~q(Prop), ~q(True) ⟩ =>
+    let intro := (.const `True.intro [])
+    let goals ← goal.apply intro
+    replaceMainGoal goals
+    return ("apply True.intro", n)
+  | ⟨1, ~q(Prop), ~q(¬$a) ⟩ =>
+    let (n, name) := getName n
+    let (_v, newGoal) ← goal.intro name.toName
+    replaceMainGoal [newGoal]
+    return (s!"intro {name}", n)
+  | ⟨1, ~q(Prop), a⟩ =>
+    let ctx ← getLCtx
+    let option_matching_expression ← ctx.findDeclM? fun decl => do
+      if decl.isImplementationDetail then return none
+      else
+        let declExpr := decl.toExpr
+        let declType ← inferType declExpr
+        if ← isDefEq declType type
+          then return some (declExpr, decl.userName)
+          else return none
+    if let some (e, userName) := option_matching_expression then
+      closeMainGoal `endSorry e
+      return (s!"exact {userName}", n)
+
+    if let some (_a, _b) := type.matchArrow? then
+      -- logInfo m!"arrow {a} {b}"
+      let (n, name) := getName n
+      let (_v, newGoal) ← goal.intro name.toName
+      replaceMainGoal [newGoal]
+      return (s!"intro {name}", n)
+    else if type.isForall then
+      let (res, n) ← forallBoundedTelescope type (some 1) (fun _fvars _body => do
+        -- logInfo m!"forall {fvars} {body}"
+        let (n, name) := getName n
+        let (_v, newGoal) ← goal.intro name.toName
+        replaceMainGoal [newGoal]
+        return (s!"intro {name}", n))
+      return (res, n)
+    else if let some (_, p) := type.app2? ``Exists then
+      let res ← lambdaBoundedTelescope p 1 (fun _fvars _body => do
+        -- logInfo m!"exists {fvars} {body}"
+        let newGoals ← goal.constructor
+        replaceMainGoal newGoals
+        -- logInfo m!"{newGoals}"
+        return s!"constructor")
+      return (res, n)
+
+    Lean.logInfo s!"By default: {a}"
+    admitGoal goal
+    return (s!"sorry", n)
+
 partial def loop (n := 0) : TacticM (String)  :=
     withMainContext do
+      let goal ← getMainGoal
 
-      try
-        let goal ← getMainGoal
-
-        let type ← goal.getType
-
-        match ← inferTypeQ type with
-        | ⟨1, ~q(Prop), ~q($a = $b) ⟩ =>
-          if ← isDefEq a b then
-            goal.refl
-            return "rfl"
-          else
-            admitGoal goal
-            return "sorry"
-        | ⟨1, ~q(Prop), ~q(True) ⟩ =>
-          let intro := (.const `True.intro [])
-          let goals ← goal.apply intro
-          replaceMainGoal goals
-          return "apply True.intro"
-        | ⟨1, ~q(Prop), ~q(¬$a) ⟩ =>
-          let (n, name) := getName n
-          let (_v, newGoal) ← goal.intro name.toName
-          replaceMainGoal [newGoal]
-          return s!"intro {name}\n{← loop n}"
-        | ⟨1, ~q(Prop), a⟩ =>
-          let ctx ← getLCtx
-          let option_matching_expression ← ctx.findDeclM? fun decl => do
-            if decl.isImplementationDetail then return none
-            else
-              let declExpr := decl.toExpr
-              let declType ← inferType declExpr
-              if ← isDefEq declType type
-                then return some (declExpr, decl.userName)
-                else return none
-          if let some (e, userName) := option_matching_expression then
-            closeMainGoal `endSorry e
-            return s!"exact {userName}"
-
-          if let some (_a, _b) := type.matchArrow? then
-            -- logInfo m!"arrow {a} {b}"
-            let (n, name) := getName n
-            let (_v, newGoal) ← goal.intro name.toName
-            replaceMainGoal [newGoal]
-            return s!"intro {name}\n{← loop n}"
-          else if type.isForall then
-            let res ← forallBoundedTelescope type (some 1) (fun _fvars _body => do
-              -- logInfo m!"forall {fvars} {body}"
-              let (n, name) := getName n
-              let (_v, newGoal) ← goal.intro name.toName
-              replaceMainGoal [newGoal]
-              return s!"intro {name}\n{← loop n}")
-            return res
-          else if let some (_, p) := type.app2? ``Exists then
-            let res ← lambdaBoundedTelescope p 1 (fun _fvars _body => do
-              -- logInfo m!"exists {fvars} {body}"
-              let newGoals ← goal.constructor
-              replaceMainGoal newGoals
-              -- logInfo m!"{newGoals}"
-              return s!"constructor\n{← loop n}")
-            return res
-
-
-          Lean.logInfo s!"By default: {a}"
-          admitGoal goal
-          return "sorry"
-      catch _e =>
-        return ""
+      let (text, n) ← process goal n
+      if ← finished then
+        return text
+      else
+        let raw ← loop n
+        return s!"{text}\n{raw}"
 
 elab_rules : tactic
   | `(tactic| sorry?) => do
--- EndSorryTest/trivial.lean
+++ EndSorryTest/trivial.lean
@@ -3,6 +3,9 @@ import EndSorry
 -- set_option pp.all true
 -- set_option autoImplicit true
 
+set_option pp.raw true
+set_option pp.raw.maxDepth 10
+
 theorem reflection {α : Type} (a : α) : a = a := by
   -- rfl
   sorry?
@@ -24,6 +27,10 @@ theorem a_true {α : Type} : α → True := by
 theorem a_then_a {α : Prop} : α → α := by
   -- intro a
   -- exact a
+  sorry?
+
+theorem direct {α : Prop} (h : α) : α := by
+  -- exact h
   sorry?
 
 theorem a_b_true {α β : Type} : α → β → True := by
@@ -54,3 +61,18 @@ theorem a_b : ∀ α, α  → True := by
 --   intro b
 --   apply True.intro
   sorry?
+
+theorem a_and_b : a → b → a ∧ b := by
+  -- intro a
+  -- intro b
+  -- constructor
+  -- exact a
+  -- exact b
+  sorry?
+
+theorem a_and_b_given (ha : a) (hb: b) : a ∧ b := by
+  -- constructor
+  -- exact ha
+  -- exact hb
+  sorry?
+
